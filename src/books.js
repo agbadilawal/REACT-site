69800a6fd410e7ecb50cb68a9b795dc48bab5d2a
sumGodbro <64108848+agbadilawal@users.noreply.github.com>
2022-06-28 12:03:10 +0100

    Create books.js

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    img: "",
+    title: "book title 1",
+    author: "book author 1",
+    id: "1",
+  },
+  {
+    img: "",
+    title: "book title 2",
+    author: "book author 2",
+    id: "2",
+  },
+  {
+    img: "",
+    title: "book title 3",
+    author: "book author 3",
+    id: "3",
+  },
+];
